efa5d7355cd8edbba51b9f0900fd9203489364b8
Serhii Frolov <larzofficialgames@gmail.com>
2026-02-11 14:51:58 +0200

    Initial commit: ASP.NET project with template

---
+++ First_Lesson_ASP/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using First_Lesson_ASP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace First_Lesson_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MyPage()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
